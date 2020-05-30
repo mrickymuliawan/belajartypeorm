dfb18230d26a60e373289240fc51988fbae5f924
Ricky <mrickymuliawan@gmail.com>
2020-05-30 10:05:07 +0700

    nambahin file

---
+++ test.js
@@ -0,0 +1,34 @@
+// class bluprint
+// new Murid(as)
+
+let murid = {
+  name: 'budi',
+  nilai: [10, 20, 30],
+  belajar: function () {
+    this.nilai.forEach(function (element) {
+      console.log(this);
+      // console.log(element);
+
+    });
+  }
+}
+// murid.belajar();
+
+class Guru {
+  name
+  address
+  static is_graduated = true
+  constructor(name) {
+    this.name = name
+  }
+  static getDataGuru() {
+    return 'data'
+  }
+}
+
+let aji = new Guru('aji')
+let tatang = new Guru('tatang')
+
+let data = DB.getDataGuru()
+console.log(Guru.is_graduated);
+
